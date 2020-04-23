38a06ab2fb33e70c909fc565d72df0efdbe0b8eb
onaimat <omarnaimat@hotmail.com>
2020-04-23 13:50:31 +0300

    commit 2

--- src/main.java
+++ src/main.java
@@ -4,6 +4,6 @@ public class main {
 
     public static void main(String[] args) {
             System.out.println("Hello Atypon");
-        System.out.println("push 2");
+           System.out.println("push 2");
     }
 }
