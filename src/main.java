432dc6c0119be748e6b0d8c16e1ebfc2a8e9ebdb
onaimat <omarnaimat@hotmail.com>
2020-04-23 03:52:32 -0700

    conflict commit

--- src/main.java
+++ src/main.java
@@ -3,7 +3,8 @@ public class main {
 
 
     public static void main(String[] args) {
-            System.out.println("Hello Atypon");
-           System.out.println("push 2");
+
+           System.out.println("Hello Atypon");
+           System.out.println("push conflict");
     }
 }
